e4d2de31ef9825f6bc8b0fdf5d5ef66aed631007
gegecouc <gegecouc@outlook.com>
2018-11-24 22:30:52 -0200

    exemplo de carga no banco para manga, personagem e pessoa

--- carga.py
+++ carga.py
@@ -10,23 +10,61 @@ from datetime import datetime
 from jikanpy import Jikan
 from pymongo import MongoClient
 
+jikan = Jikan()
+
 # inicia o cliente
 client = MongoClient()
 # nome do bd
 db = client.t2
-# nome da coleção
+
+# nome da coleção do anime no mongo
 anime = db.anime
 
-jikan = Jikan()
-
-for x in range(1, 10): # range teste
+for x in range(1, 10):  # range teste
     r = requests.get("https://api.jikan.moe/v3/anime/" + str(x))
     try:
         newAnime = jikan.anime(x)  # passar o json do anime com id x para variável
         print("Ok")
         # anime.insert_one(newAnime)  # Inserir anime na coleção do mongo db
-        
-
-    except jikanpy.exceptions.APIException as e: #erro 4x na API
+    except jikanpy.exceptions.APIException as e:  # erro 4x na API
         print("Anime com id " + str(x) + " não existe")  # anime sem ID
+    t.sleep(3)
+
+# nome da coleção do manga no mongo
+manga = db.manga
+
+for x in range(1, 10):  # range teste
+    r = requests.get("https://api.jikan.moe/v3/manga/" + str(x))
+    try:
+        newManga = jikan.manga(x)  # passar o json do manga com id x para variável
+        print("Ok")
+        # manga.insert_one(newManga)  # Inserir manga na coleção do mongo db
+    except jikanpy.exceptions.APIException as e:  # erro 4x na API
+        print("Manga com id " + str(x) + " não existe")  # manga sem ID
+    t.sleep(3)
+
+# nome da coleção do personagem no mongo
+personagem = db.personagem
+
+for x in range(1, 10):  # range teste
+    r = requests.get("https://api.jikan.moe/v3/character/" + str(x))
+    try:
+        newCharacter = jikan.character(x)  # passar o json do personagem com id x para variável
+        print("Ok")
+        # manga.insert_one(newCharacter)  # Inserir personagem na coleção do mongo db
+    except jikanpy.exceptions.APIException as e:  # erro 4x na API
+        print("Personagem com id " + str(x) + " não existe")  # personagem sem ID
+    t.sleep(3)
+
+# nome da coleção da pessoa no mongo
+pessoa = db.pessoa
+
+for x in range(1, 10):  # range teste
+    r = requests.get("https://api.jikan.moe/v3/person/" + str(x))
+    try:
+        newPessoa = jikan.person(x)  # passar o json da pessoa com id x para variável
+        print("Ok")
+        # pessoa.insert_one(newPessoa)  # Inserir pessoa na coleção do mongo db
+    except jikanpy.exceptions.APIException as e:  # erro 4x na API
+        print("Pessoa com id " + str(x) + " não existe")  # pessoa sem ID
     t.sleep(3)
